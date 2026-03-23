3caaa609e26bf77ced956b0758e06f6de8ff446c
Aelly-A <aellyalwardi@gmail.com>
2026-03-23 15:31:30 -0400

    remove db due to file size and fix data merger

--- combine_sqlite_dbs.py
+++ combine_sqlite_dbs.py
@@ -1,35 +1,23 @@
 from os.path import isfile, isdir, join
+from pandas import read_csv, read_sql
 from sqlite3 import connect
-from pandas import read_csv
 from os import listdir
 
-def merge_news_data():
-    news_dir = "news_data"
-    output_file = "news_data/ai_articles.csv"
+def merge_news_data(news_dir="news_data", output_db="db.sqlite"):
+    conn = connect(output_db)
 
-    if not isfile(output_file):
-        with open(output_file, "w", encoding="utf-8") as f:
-            f.write("url,date,title,text,sitename\n")
-        for folder in sorted(listdir(news_dir)):
-            dirpath = join(news_dir, folder)
-            if not isdir(dirpath):
+    for folder in sorted(listdir(news_dir)):
+        dirpath = join(news_dir, folder)
+        if not isdir(dirpath):
+            continue
+
+        for file in listdir(dirpath):
+            filepath = join(dirpath, file)
+            if not file.endswith(".csv"):
                 continue
 
-            for file in listdir(dirpath):
-                filepath = join(dirpath, file)
-                if not file.endswith(".csv"):
-                    continue
-
-                with open(filepath, "r", encoding="utf-8") as f:
-                    file_lines = f.readlines()[1:] #[1:] to remove header
-
-                with open(output_file, "a", encoding="utf-8") as f:
-                    f.writelines(file_lines)
-
-    csv_df = read_csv(output_file)
-    conn = connect("db.sqlite")
-    csv_df.to_sql("Articles", con=conn, if_exists="replace", index=False)
-
+            csv_file = read_csv(filepath)
+            csv_file.to_sql("Articles", con=conn, if_exists="append", index=False)
 
 
 def main(source=None, dest=None):
@@ -39,41 +27,17 @@ def main(source=None, dest=None):
     if not dest:
         dest = input("Enter the destination sqlite database name: ")
 
+    layoffs = read_csv("layoffs.csv")
     conn = connect(source)
-    c = conn.cursor()
-    c.execute('select * from articles')
-    rows = c.fetchall()
-    c.close()
+    devposts = read_sql("SELECT * FROM articles", conn)
     conn.close()
 
     conn = connect(dest)
-    c = conn.cursor()
-
-    c.execute("""
-              CREATE TABLE IF NOT EXISTS DevPosts (
-                  post_id INT PRIMARY KEY,
-                  title TEXT,
-                  url TEXT,
-                  published_at DATE,
-                  tags TEXT,
-                  body TEXT
-              ) """)
-
-    for row in rows:
-        post_id, title, url, published_at, tags, body_text, impressions = row
-        tags = tags.replace(",", "").replace(" ", ", ")
-
-        c.execute("INSERT OR IGNORE INTO DevPosts VALUES (?, ?, ?, ?, ?, ?)",
-                  (post_id, title, url, published_at, tags, body_text))
-
-    layoffs = read_csv("layoffs.csv")
-    layoffs.to_sql("Layoffs", con=conn, if_exists="replace", index=False)
-
-    conn.commit()
-
-
+    devposts.to_sql("DevPosts", con=conn, if_exists="delete_rows", index=False)
+    layoffs.to_sql("Layoffs", con=conn, if_exists="delete_rows", index=False)
+    conn.close()
 
 
 if __name__ == "__main__":
     merge_news_data()
-    main(source="dev_ai_articles_full.sqlite", dest="db.sqlite")
+    main(source="dev_notebooks/dev_ai_articles_full.sqlite", dest="db.sqlite")
